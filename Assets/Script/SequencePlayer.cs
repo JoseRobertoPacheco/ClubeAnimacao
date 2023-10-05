a58a568a71333a9a784d233d0cd6ca878278d4d8
coolergate <107399022+coolergate@users.noreply.github.com>
2023-10-05 18:12:58 -0300

    add cel shading and delivery van model

--- Assets/Script/SequencePlayer.cs
+++ Assets/Script/SequencePlayer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,61 +7,68 @@ public class SequencePlayer : MonoBehaviour
 	public GameObject FadeLogoObject;
 
 	public TextMeshProUGUI Logo1;
-    public TextMeshProUGUI Logo2;
+	public TextMeshProUGUI Logo2;
 	public TextMeshProUGUI Logo3;
 
-    // Start is called before the first frame update
-    void Start()
-	{
-        Logo1.gameObject.SetActive(false);
-        Logo2.gameObject.SetActive(false);
-        Logo3.gameObject.SetActive(false);
+	public Camera LocalCamera;
 
-        StartCoroutine(StartAnimation());
+	IEnumerator RotateWheels()
+	{
+		while (true)
+		{
+			
+		}
 	}
-	
+
+	void Start()
+	{
+		Logo1.gameObject.SetActive(false);
+		Logo2.gameObject.SetActive(false);
+		Logo3.gameObject.SetActive(false);
+
+		StartCoroutine(StartAnimation());
+	}
+
 	IEnumerator StartAnimation()
 	{
 		yield return new WaitForSeconds(5);
 
 		Logo1.gameObject.SetActive(true);
 
-        yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds(2);
 
-        Logo2.gameObject.SetActive(true);
+		Logo2.gameObject.SetActive(true);
 
-        yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds(2);
 
-		LeanTween.move(FadeLogoObject, new Vector3(0, 0, 0), 3);
+		LeanTween.move(FadeLogoObject, new Vector3(0, 0, 0), 3).setEase(LeanTweenType.easeInOutQuad);
 
-        yield return new WaitForSeconds(6);
+		yield return new WaitForSeconds(4);
 
-        Logo1.gameObject.SetActive(false);
-        Logo2.gameObject.SetActive(false);
-		FadeLogoObject.SetActive(false);
+		Logo1.gameObject.SetActive(false);
+		Logo2.gameObject.SetActive(false);
+		Logo3.gameObject.SetActive(true);
 
-		void UpdateTextColor(Color color)
+		LeanTween.move(FadeLogoObject, new Vector3(0, -20, 0), 3).setEase(LeanTweenType.easeInOutQuad);
+
+		yield return new WaitForSeconds(4);
+
+        FadeLogoObject.SetActive(false);
+
+        void UpdateTextColor(Color color)
 		{
 			Logo3.color = color;
 		}
+		LeanTween.value(Logo3.gameObject, UpdateTextColor, Logo3.color, new Color(1, 1, 1, 0f), 1f);
 
-		float OriginalZ = Logo3.gameObject.transform.position.z;
-		Logo3.gameObject.transform.position = new Vector3(Logo3.gameObject.transform.position.x, Logo3.gameObject.transform.position.y, 0);
+		yield return new WaitForSeconds(1);
 
-        Logo3.color = new Color(1, 1, 1, 0f);
-        Logo3.gameObject.SetActive(true);
-
-		LeanTween.value(gameObject, UpdateTextColor, Logo3.color, new Color(1, 1, 1, 1f), 2);
-        LeanTween.moveZ(Logo3.gameObject, OriginalZ, 6);
-
-        yield return new WaitForSeconds(4);
-
-        LeanTween.value(Logo3.gameObject, UpdateTextColor, Logo3.color, new Color(1, 1, 1, 0f), 2);
-    }
+		Logo3.gameObject.SetActive(false);
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+
 	}
 }
