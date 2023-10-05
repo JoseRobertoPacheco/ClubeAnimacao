16bc9619613fa9710797bd3cf6bd4cabb9a7a1a0
coolergate <107399022+coolergate@users.noreply.github.com>
2023-10-05 12:52:47 -0300

    update intro scene

--- Assets/Script/SequencePlayer.cs
+++ Assets/Script/SequencePlayer.cs
@@ -39,18 +39,25 @@ public class SequencePlayer : MonoBehaviour
 
         Logo1.gameObject.SetActive(false);
         Logo2.gameObject.SetActive(false);
+		FadeLogoObject.SetActive(false);
 
 		void UpdateTextColor(Color color)
 		{
 			Logo3.color = color;
 		}
 
-		Logo3.color = new Color(1, 1, 1, 0f);
+		float OriginalZ = Logo3.gameObject.transform.position.z;
+		Logo3.gameObject.transform.position = new Vector3(Logo3.gameObject.transform.position.x, Logo3.gameObject.transform.position.y, 0);
+
+        Logo3.color = new Color(1, 1, 1, 0f);
         Logo3.gameObject.SetActive(true);
 
 		LeanTween.value(gameObject, UpdateTextColor, Logo3.color, new Color(1, 1, 1, 1f), 2);
+        LeanTween.moveZ(Logo3.gameObject, OriginalZ, 6);
 
-		yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(4);
+
+        LeanTween.value(Logo3.gameObject, UpdateTextColor, Logo3.color, new Color(1, 1, 1, 0f), 2);
     }
 
 	// Update is called once per frame
